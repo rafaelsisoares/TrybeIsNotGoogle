3a5ee05d6ce73bddd146ea24b9cb6ac9140b5266
Rafael Soares <r.soares2015@outlook.com>
2023-06-06 23:31:07 -0300

    feat: Implementa a funcao txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido')
+    try:
+        with open(path_file, mode='r') as file:
+            return [line.strip('\n') for line in file]
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
