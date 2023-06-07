633ea75b0fd423df9f8a5491097062fa5cac12f2
Rafael Soares <r.soares2015@outlook.com>
2023-06-07 00:48:12 -0300

    feat: Implementa a funcao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    file_processed = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines
+    }
+    for i in range(len(instance)):
+        if instance.search(i) == file_processed:
+            return
+    instance.enqueue(file_processed)
+    print(file_processed)
 
 
 def remove(instance):
