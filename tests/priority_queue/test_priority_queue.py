5d11f126500ba4c381ce1d44ac445f8d78fbb14d
Rafael Soares <r.soares2015@outlook.com>
2023-06-09 00:29:09 -0300

    test: Implementa testes para a classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,30 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
-def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+def test_basic_priority_queueing(capsys):
+    queue_with_priority = PriorityQueue()
+    priority_file = {
+        'nome_do_arquivo': 'Indy',
+        'qtd_linhas': 2,
+        'linhas_do_arquivo': ['Indianapolis', '500']
+    }
+    normal_file = {
+        'nome_do_arquivo': 'Le_Mans',
+        'qtd_linhas': 5,
+        'linhas_do_arquivo': ['24', 'horas', 'de', 'le', 'mans']
+    }
+
+    queue_with_priority.enqueue(priority_file)
+    queue_with_priority.enqueue(normal_file)
+    assert len(queue_with_priority) == 2
+
+    assert queue_with_priority.search(0) == priority_file
+    assert queue_with_priority.search(1) == normal_file
+    with pytest.raises(IndexError, match='Índice Inválido ou Inexistente'):
+        queue_with_priority.search(2)
+
+    assert queue_with_priority.is_priority(normal_file) is False
+
+    assert queue_with_priority.dequeue() == priority_file
+    assert len(queue_with_priority) == 1
