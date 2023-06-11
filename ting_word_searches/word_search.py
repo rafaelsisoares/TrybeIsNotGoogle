73ecc9cbbe46eef5b68c77cb3f406bef7479da3d
Rafael Soares <r.soares2015@outlook.com>
2023-06-11 02:21:32 -0300

    feat: Implementa a funcao exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,32 @@
+def process_info(word, lines, is_full_info):
+    info = []
+    for i in range(len(lines)):
+        if word.lower() in lines[i].lower():
+            if is_full_info:
+                info.append({
+                    'linha': i + 1,
+                    'conteudo': lines[i],
+                })
+            info.append({'linha': i + 1})
+
+    return info
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    for i in range(len(instance)):
+        info = process_info(
+            word,
+            instance.search(i)['linhas_do_arquivo'],
+            False
+        )
+        if info:
+            results.append({
+                'palavra': word,
+                'arquivo': instance.search(i)['nome_do_arquivo'],
+                'ocorrencias': info
+            })
+    return results
 
 
 def search_by_word(word, instance):
